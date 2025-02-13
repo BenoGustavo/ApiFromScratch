63a4ce5f6a7bcf99177cc991c9da0482257181fe
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-13 17:14:03 -0300

    enhance Router class to support dynamic route parameters and improve request handling

--- src/routes/custom-router.js
+++ src/routes/custom-router.js
@@ -3,30 +3,61 @@ class Router {
 		this.routes = [];
 	}
 
+	#buildRoutePath(path) {
+		const routeParametersRegex = /:([a-zA-Z]+)/g;
+		const pathWithParams = path.replaceAll(
+			routeParametersRegex,
+			"([a-z0-9-_]+)"
+		);
+		const pathRegex = new RegExp(`^${pathWithParams}$`);
+
+		return pathRegex;
+	}
+
 	get(url, callback) {
-		this.routes.push({ method: "GET", url, callback });
+		this.routes.push({
+			method: "GET",
+			url: this.#buildRoutePath(url),
+			callback,
+		});
 	}
 
 	post(url, callback) {
-		this.routes.push({ method: "POST", url, callback });
+		this.routes.push({
+			method: "POST",
+			url: this.#buildRoutePath(url),
+			callback,
+		});
 	}
 
 	put(url, callback) {
-		this.routes.push({ method: "PUT", url, callback });
+		this.routes.push({
+			method: "PUT",
+			url: this.#buildRoutePath(url),
+			callback,
+		});
 	}
 
 	patch(url, callback) {
-		this.routes.push({ method: "PATCH", url, callback });
+		this.routes.push({
+			method: "PATCH",
+			url: this.#buildRoutePath(url),
+			callback,
+		});
 	}
 
 	delete(url, callback) {
-		this.routes.push({ method: "DELETE", url, callback });
+		this.routes.push({
+			method: "DELETE",
+			url: this.#buildRoutePath(url),
+			callback,
+		});
 	}
 
 	handleRequest(req, res) {
-		const route = this.routes.find(
-			(route) => route.method === req.method && route.url === req.url
-		);
+		const route = this.routes.find((route) => {
+			return route.method === req.method && route.url.test(req.url);
+		});
 
 		if (route) {
 			return route.callback(req, res);
